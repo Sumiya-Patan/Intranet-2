2a35ae4395cc6b949690b22358dbd7d4674bb557
Ajaybhukya <ajaykumarbhukya511@gmail.com>
2025-10-14 15:03:34 +0530

    initial config

--- src/main/java/com/intranet/repository/TimeSheetEntryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.intranet.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.intranet.entity.TimeSheetEntry;
-
-@Repository
-public interface TimeSheetEntryRepo extends JpaRepository<TimeSheetEntry, Long> {}
