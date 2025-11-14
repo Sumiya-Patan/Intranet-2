7f65bf67644da3fb626a10cc9b99cf9bbaa3da00
Ajaybhukya <ajaykumarbhukya511@gmail.com>
2025-11-14 15:29:15 +0530

    Monthly controller fixes

--- src/main/java/com/intranet/controller/reports/MonthlyUserReportController.java
+++ src/main/java/com/intranet/controller/reports/MonthlyUserReportController.java
@@ -1,23 +1,31 @@
 package com.intranet.controller.reports;
 
 import com.intranet.dto.MonthlyUserReportDTO;
+import com.intranet.dto.UserDTO;
+import com.intranet.security.CurrentUser;
 import com.intranet.service.MonthlyUserReportService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/reports")
+@RequestMapping("/api/report")
 @RequiredArgsConstructor
 public class MonthlyUserReportController {
 
     private final MonthlyUserReportService reportService;
 
-    @GetMapping("/user/monthly")
-    public MonthlyUserReportDTO getMonthlyReport(
+    @GetMapping("/user_monthly")
+    public ResponseEntity<?> getMonthlyReport(
+            @CurrentUser UserDTO currentUser,
             @RequestParam int month,
             @RequestParam int year) {
-        
-
-        return reportService.getMonthlyUserReport(17L, month, year);
+        try {
+        MonthlyUserReportDTO report = reportService.getMonthlyUserReport(currentUser.getId(), month, year);
+        return ResponseEntity.ok(report);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error generating report: " + e.getMessage());
+        }
     }
 }
