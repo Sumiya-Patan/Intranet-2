73ba52343e4c0779c80646ef610eb4ef3da8e5a7
Ajaybhukya <ajaykumarbhukya511@gmail.com>
2025-12-02 15:34:26 +0530

    stopped full holiday cronjob

--- src/main/java/com/intranet/service/cornjobs/CompleteWeekHoliday/MonthlyFullHolidayWeekCron.java
+++ src/main/java/com/intranet/service/cornjobs/CompleteWeekHoliday/MonthlyFullHolidayWeekCron.java
@@ -3,7 +3,6 @@ package com.intranet.service.cornjobs.CompleteWeekHoliday;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,7 +15,7 @@ public class MonthlyFullHolidayWeekCron {
      * Runs monthly on 1st at 1 AM
      * Cron: second minute hour day-of-month month day-of-week
      */
-    @Scheduled(cron = "0 0 1,2 1,2,3 * ?", zone = "Asia/Kolkata")
+    // @Scheduled(cron = "0 0 1,2 1,2,3 * ?", zone = "Asia/Kolkata")
     // @Scheduled(cron = "0 */2 * * * *")
     public void runMonthlyFullHolidayProcessing() {
         System.out.println("🟢 Monthly Full Holiday Week Cron Started...");
