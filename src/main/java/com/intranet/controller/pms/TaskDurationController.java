6ca6eb1dd1e4017d405eeb2015c70e0be8831e96
Ajaybhukya <ajaykumarbhukya511@gmail.com>
2025-12-03 14:57:58 +0530

    PMS Endpoinnts origin

--- src/main/java/com/intranet/controller/pms/TaskDurationController.java
+++ src/main/java/com/intranet/controller/pms/TaskDurationController.java
@@ -3,6 +3,7 @@ package com.intranet.controller.pms;
 import java.time.LocalDate;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/tasks")
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 @RequiredArgsConstructor
 public class TaskDurationController {
 
