2a976c0d9585314b0aaaf86ba041c1d9e7630c67
Aditya <adityateja0118@gmail.com>
2026-04-20 12:07:21 +0530

    projects

--- src/main/java/com/intranet/controller/RMS/RMSProjectHoursSummaryController.java
+++ src/main/java/com/intranet/controller/RMS/RMSProjectHoursSummaryController.java
@@ -3,9 +3,11 @@ package com.intranet.controller.RMS;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.intranet.dto.rms.RMSProjectDetailResponseDTO;
 import com.intranet.dto.rms.RMSProjectHoursSummaryResponseDTO;
 import com.intranet.service.RMS.RMSProjectHoursSummaryService;
 
@@ -24,5 +26,11 @@ public class RMSProjectHoursSummaryController {
     @Operation(summary = "Get all projects with planned, actual, pending, utilization, billable, non-billable, and resource hours")
     public ResponseEntity<RMSProjectHoursSummaryResponseDTO> getProjectHoursSummary() {
         return ResponseEntity.ok(service.getProjectHoursSummary());
+    }
+
+    @GetMapping("/RMS/project-hours-summary/{projectId}")
+    @Operation(summary = "Get one timesheet project detail with full project info, resources, and tasks")
+    public ResponseEntity<RMSProjectDetailResponseDTO> getProjectDetail(@PathVariable Long projectId) {
+        return ResponseEntity.ok(service.getProjectDetail(projectId));
     }
 }
---
+++ src/main/java/com/intranet/dto/rms/RMSProjectMemberDTO.java
@@ -0,0 +1,10 @@
+package com.intranet.dto.rms;
+
+import lombok.Data;
+
+@Data
+public class RMSProjectMemberDTO {
+    private Long resourceId;
+    private String resourceName;
+    private String resourceEmail;
+}
